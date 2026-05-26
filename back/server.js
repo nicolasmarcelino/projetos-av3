9b59727f5c83fa78cdcb05350dcc9f6b3962b053
Nicolas Marcelino <nicolasmarcelinomarcelino@yahoo.com.br>
2026-05-26 15:20:37 -0300

    Primeiro commit

---
+++ back/server.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const express = require('express');
+const { createClient } = require('@supabase/supabase-js');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_PUBLISHABLE_KEY
+);
+
+app.get('/', (req, res) => {
+  res.send('Sistema de compras');
+});
+
+app.get('/produtos', async (req, res) => {
+  try {
+    const { data, error } = await supabase
+      .from('produto')
+      .select();
+
+    if (error) throw error;
+
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ front/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <h1>Sistema de compras</h1>
+    </main>
+  )
+}
+
+export default App
