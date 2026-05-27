a9de2adf0235c1f6ec45d14132f2bc28f4380c90
Nicolas Marcelino <nicolasmarcelinomarcelino@yahoo.com.br>
2026-05-27 01:03:08 -0300

    Correção CORS

--- back/server.js
+++ back/server.js
@@ -1,10 +1,20 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
+
 const { createClient } = require('@supabase/supabase-js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+/* corsOptios é necessário para permitir as requisições vindas do frontend */
+
+const corsOptions = {
+  origin: 'http://localhost:5173'
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
